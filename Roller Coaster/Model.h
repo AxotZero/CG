b5b6e6e3162822132d2843819356d861a87c0d6f
Axot <asdswegreh@gmail.com>
2019-01-08 23:51:39 +0800

    少加一個檔案

--- Roller Coaster/Model.h
+++ Roller Coaster/Model.h
@@ -1,51 +1,32 @@
 #ifndef MODEL_H
 #define MODEL_H
-#include <QtGui/QOpenGLFunctions_4_3_Core>
-#include <QtGui/QOpenGLVertexArrayObject>
-#include <QtGui/QOpenGLBuffer>
-#include <QtGui/QOpenGLShader>
-#include <QtGui/QOpenGLShaderProgram>
-#include <QVector>
-#include <QVector3D>
-#include <QFileInfo>
-#include <QDebug>
-#include <QString>
+
 #include <QtCore/QString>
 #include <QtCore/QVector>
+
 #include <math.h>
+
 #include "point3d.h"
 
 class Model
 {
 public:
 	Model() {}
-	Model(const QString &filePath, int s, Point3d p);
+	Model(const QString &filePath, float s, Point3d p, bool ifcenter);
 
-	void Paint(GLfloat* ProjectionMatrix, GLfloat* ModelViewMatrix);
+	void render(bool wireframe = false, bool normals = false) const;
 
 	QString fileName() const { return m_fileName; }
 	int faces() const { return m_pointIndices.size() / 3; }
 	int edges() const { return m_edgeIndices.size() / 2; }
 	int points() const { return m_points.size(); }
-	void Init();
-	void InitVAO();
-	void InitVBO();
-	void InitShader(QString vertexShaderPath, QString fragmentShaderPath);
-private:
-	QOpenGLShaderProgram * shaderProgram;
-	QOpenGLShader* vertexShader;
-	QOpenGLShader* fragmentShader;
-	QOpenGLVertexArrayObject vao;
-	QOpenGLBuffer vvbo;
-	QOpenGLBuffer cvbo;
 
+private:
 	QString m_fileName;
 	QVector<Point3d> m_points;
 	QVector<Point3d> m_normals;
 	QVector<int> m_edgeIndices;
 	QVector<int> m_pointIndices;
-	QVector<QVector3D> vertices;
-	QVector<QVector3D> normals;
 };
 
 #endif
